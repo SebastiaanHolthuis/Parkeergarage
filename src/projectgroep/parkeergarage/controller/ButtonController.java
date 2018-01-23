1125fa8dbe0e6a963e06741fa37cdac73c783c38
Jesper <jesper.honken@gmail.com>
2018-01-23 15:09:51 +0100

    Controller

--- src/projectgroep/parkeergarage/controller/ButtonController.java
+++ src/projectgroep/parkeergarage/controller/ButtonController.java
@@ -3,34 +3,14 @@ package projectgroep.parkeergarage.controller;
 import java.awt.event.*;
 import javax.swing.*;
 
+import projectgroep.parkeergarage.logic.ParkeerLogic;
+
 
 public class ButtonController extends AbstractController implements ActionListener {
 
-	private JButton stepOne;
-	private JButton startSteps;
-	private JButton stopSteps;
-	private JTextField steps;
 	
-	public ButtonController() {
-		setSize(450, 50);
-		stepOne=new JButton("One step");
-		stepOne.addActionListener(this);
-		steps=new JTextField();
-		startSteps=new JButton("Start");
-		startSteps.addActionListener(this);
-		stopSteps=new JButton("Stop");
-		stopSteps.addActionListener(this);
-		
-		this.setLayout(null);
-		add(stepOne);
-		add(steps);
-		add(startSteps);
-		add(stopSteps);
-		stepOne.setBounds(50, 10, 70, 30);
-		steps.setBounds(140, 10, 70, 30);
-		startSteps.setBounds(229, 10, 70, 30);
-		stopSteps.setBounds(319, 10, 70, 30);
 
-		setVisible(true);
-}
+public void actionPerformed(ActionEvent e) {
+	
+	}
 }
