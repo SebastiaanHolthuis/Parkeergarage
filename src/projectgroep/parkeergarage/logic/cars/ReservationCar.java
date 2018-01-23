d8f6d4ae376f4d20ed42b6dc455640aa4729ec87
reinvdwoerd <reinvanderwoerd@me.com>
2018-01-23 16:04:12 +0100

    teal

--- src/projectgroep/parkeergarage/logic/cars/ReservationCar.java
+++ src/projectgroep/parkeergarage/logic/cars/ReservationCar.java
@@ -4,19 +4,19 @@ import java.util.Random;
 import java.awt.*;
 
 public class ReservationCar extends Car {
-	private static final Color COLOR=Color.decode("#FFFF00");
-	
-    public ReservationCar(int priceToPay) {
-    	super(priceToPay);
+    public static final Color COLOR = Color.decode("#34D1BF");
 
-    	Random random = new Random();
-    	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
+    public ReservationCar(int priceToPay) {
+        super(priceToPay);
+
+        Random random = new Random();
+        int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
         this.setMinutesLeft(stayMinutes);
         this.setHasToPay(true);
         this.setPriceToPay(6);
     }
-    
-    public Color getColor(){
-    	return COLOR;
+
+    public Color getColor() {
+        return COLOR;
     }
 }
