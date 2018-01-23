c11efc5644360082a23d651f7832e436eba63f35
reinvdwoerd <reinvanderwoerd@me.com>
2018-01-23 16:17:23 +0100

    PieChartView teal

--- src/projectgroep/parkeergarage/view/PieChartView.java
+++ src/projectgroep/parkeergarage/view/PieChartView.java
@@ -10,6 +10,7 @@ import projectgroep.parkeergarage.logic.ParkeerLogic;
 import projectgroep.parkeergarage.logic.cars.AdHocCar;
 import projectgroep.parkeergarage.logic.cars.Car;
 import projectgroep.parkeergarage.logic.cars.ParkingPassCar;
+import projectgroep.parkeergarage.logic.cars.ReservationCar;
 
 public class PieChartView extends AbstractView {
 
@@ -47,26 +48,26 @@ public class PieChartView extends AbstractView {
         drawChart(graphics);
 
         repaint();
-	}
-	
-	public void drawChart(Graphics g) {	
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, 1000, 1000);
-		
-		// Cirkel
-		g.setColor(Color.LIGHT_GRAY);
-		g.fillArc(12, 10, 180, 180, 0, 360);
-		
-		// Parking pass
-		g.setColor(ParkingPassCar.COLOR);
-		g.fillArc(12, 10, 180, 180, 0, (int) model.getParkingPassCars().count() - (int) model.getAdHocCars().count());
-		
-		// Ad Hoc
-		g.setColor(AdHocCar.COLOR);
-		g.fillArc(12, 10, 180, 180, 0, (int) model.getAdHocCars().count());
-		
-		// Reservatie auto
-		g.setColor(Color.decode("#FFFF00"));
-		g.fillArc(12, 10, 180, 180, 0, (int) model.getReservationCars().count());
-	}
+    }
+
+    public void drawChart(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 1000, 1000);
+
+        // Cirkel
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillArc(12, 10, 180, 180, 0, 360);
+
+        // Parking pass
+        g.setColor(ParkingPassCar.COLOR);
+        g.fillArc(12, 10, 180, 180, 0, (int) model.getParkingPassCars().count() - (int) model.getAdHocCars().count());
+
+        // Ad Hoc
+        g.setColor(AdHocCar.COLOR);
+        g.fillArc(12, 10, 180, 180, 0, (int) model.getAdHocCars().count());
+
+        // Reservatie auto
+        g.setColor(ReservationCar.COLOR);
+        g.fillArc(12, 10, 180, 180, 0, (int) model.getReservationCars().count());
+    }
 }
