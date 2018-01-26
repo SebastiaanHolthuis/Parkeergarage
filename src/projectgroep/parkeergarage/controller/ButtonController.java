315da1905e52604e3f3d101a21cb624ac8adddab
reinvdwoerd <reinvanderwoerd@me.com>
2018-01-26 07:29:05 +0100

    lambda's in ButtonController

--- src/projectgroep/parkeergarage/controller/ButtonController.java
+++ src/projectgroep/parkeergarage/controller/ButtonController.java
@@ -9,56 +9,39 @@ import projectgroep.parkeergarage.main.Simulator;
 
 @SuppressWarnings("serial")
 public class ButtonController extends AbstractController {
-	 JButton Apply = new JButton("Start");
-	 JButton OneStep = new JButton("+1");
-     JButton Stop = new JButton("Stop");     
-     JButton StepBack = new JButton("Vorige stap");
-     
-     JButton Reset = new JButton("Reset");
-     ParkeerLogic parkeerLogic;
-     Simulator sim;
-     ParkeerLogic tickfor;
-     ParkeerLogic tickback;
-     
-	public ButtonController(Simulator simulator, ParkeerLogic parkeerLogic) {
-		this.parkeerLogic = parkeerLogic;
-		this.sim = simulator;
-		 Apply.setBounds(10, 572, 73, 26);
-		 simulator.getScreen().getContentPane().add(Apply);
-		 Apply.addActionListener(this);
-		 
-	     Stop.setBounds(141, 572,73, 26);
-	     simulator.getScreen().getContentPane().add(Stop);
-	     Stop.addActionListener(this);
+    JButton Apply = new JButton("Start");
+    JButton OneStep = new JButton("+1");
+    JButton Stop = new JButton("Stop");
+    JButton StepBack = new JButton("Vorige stap");
 
-	     OneStep.setBounds(144, 624, 73, 23);
-	     simulator.getScreen().getContentPane().add(OneStep);
-	     OneStep.addActionListener(this);
-	    
-	     StepBack.setBounds(10, 624, 73, 23);
-	     simulator.getScreen().getContentPane().add(StepBack);
-	     StepBack.addActionListener(this);
-	     
-	     Reset.setBounds(66, 681, 89, 23);
-	     simulator.getScreen().getContentPane().add(Reset);
-	     Reset.addActionListener(this);
-	}
+    JButton Reset = new JButton("Reset");
+    ParkeerLogic parkeerLogic;
+    Simulator sim;
+    ParkeerLogic tickfor;
+    ParkeerLogic tickback;
 
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == Stop) {
-			parkeerLogic.stop();
-		}
-		if (e.getSource() == Reset) {
-			sim.restart(parkeerLogic.settings);
-		}
-		if (e.getSource() == Apply) {
-			parkeerLogic.run();
-		}
-		if (e.getSource() == OneStep) {
-			parkeerLogic.tickSimulator();
-		}
-		if (e.getSource() == StepBack) {
-			parkeerLogic.GetHistory();
-		}
-	}
+    public ButtonController(Simulator simulator, ParkeerLogic parkeerLogic) {
+        this.parkeerLogic = parkeerLogic;
+        this.sim = simulator;
+        Apply.setBounds(10, 572, 73, 26);
+        simulator.getScreen().getContentPane().add(Apply);
+
+        Stop.setBounds(141, 572, 73, 26);
+        simulator.getScreen().getContentPane().add(Stop);
+
+        OneStep.setBounds(144, 624, 73, 23);
+        simulator.getScreen().getContentPane().add(OneStep);
+
+        StepBack.setBounds(10, 624, 73, 23);
+        simulator.getScreen().getContentPane().add(StepBack);
+
+        Reset.setBounds(66, 681, 89, 23);
+        simulator.getScreen().getContentPane().add(Reset);
+
+        Apply.addActionListener(e -> parkeerLogic.run());
+        Stop.addActionListener(e -> parkeerLogic.stop());
+        OneStep.addActionListener(e -> parkeerLogic.tickSimulator());
+        StepBack.addActionListener(e -> parkeerLogic.GetHistory());
+        Reset.addActionListener(e -> sim.restart(parkeerLogic.settings));
+    }
 }
