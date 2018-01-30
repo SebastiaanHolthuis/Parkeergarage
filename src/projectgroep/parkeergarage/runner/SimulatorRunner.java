dd13a94cece25680a237ea553f26c1725d0ce07a
reinvdwoerd <reinvanderwoerd@me.com>
2018-01-30 20:23:53 +0100

    Losse JavaFX panel

--- src/projectgroep/parkeergarage/runner/SimulatorRunner.java
+++ src/projectgroep/parkeergarage/runner/SimulatorRunner.java
@@ -1,12 +1,33 @@
 package projectgroep.parkeergarage.runner;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import projectgroep.parkeergarage.SettingsRepository;
 import projectgroep.parkeergarage.main.Simulator;
 
-public class SimulatorRunner {
+public class SimulatorRunner extends Application {
     public static void main(String[] args) {
-    	Simulator simulator = new Simulator(SettingsRepository.loadSettings());
+        new Thread(() -> launch()).start();
+
+        Simulator simulator = new Simulator(SettingsRepository.loadSettings());
         simulator.getParkeerLogic().run();
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
