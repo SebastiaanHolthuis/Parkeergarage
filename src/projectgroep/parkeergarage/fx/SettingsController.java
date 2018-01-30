d0e6e3a32748927f7e3e00e941ec213bae82af99
reinvdwoerd <reinvanderwoerd@me.com>
2018-01-30 22:17:20 +0100

    Restart button

--- src/projectgroep/parkeergarage/fx/SettingsController.java
+++ src/projectgroep/parkeergarage/fx/SettingsController.java
@@ -10,23 +10,34 @@ import java.util.HashMap;
 import java.util.List;
 
 public class SettingsController {
-
     @FXML
     GridPane container;
 
+    private HashMap<String, TypedTextField> fields = new HashMap<>();
+
     public void initialize() {
-        System.out.println();
-
-        HashMap<String, Object> map = (Simulator.model.settings).asMap();
-
-        List<String> keyList = new ArrayList<String>(map.keySet());
+        HashMap<String, Object> settings = (Simulator.model.settings).asMap();
+        List<String> keyList = new ArrayList<>(settings.keySet());
         for (int i = 0; i < keyList.size(); i++) {
             Label label = new Label(keyList.get(i));
-            TextField field = new TypedTextField(map.get(keyList.get(i)));
+            TextField field = new TypedTextField(settings.get(keyList.get(i)));
 
             container.add(label, 0, i);
             container.add(field, 1, i);
         }
+    }
+
+
+    private HashMap<String, Object> getSettingsMap() {
+        HashMap<String, Object> results = new HashMap<>();
+        fields.forEach((setting, field) ->
+                results.put(setting, field.getValue()));
+        System.out.println(results.toString());
+        return results;
+    }
+
+    private void restart() {
+        System.out.println(getSettingsMap());
     }
 
 }
